bd60e66d924dd6c2bac5bd008d285368b91a1529
kunalpratapsinghh <1,3kunal.tt@gmail.com>
2022-11-02 12:55:33 +0530

    asdas

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json())
 app.use("",router)
 
-app.listen(8080,async(req,res)=>{
+app.listen(process.env.PORT || 8080 ,async(req,res)=>{
     try {
         await connection
         console.log("Connected")
